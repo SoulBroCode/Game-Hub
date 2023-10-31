743635866b8c30aefb44c5f3123080fbfd448094
SoulBroCode <linhhuynh2000@gmail.com>
2023-10-31 20:26:53 +0000

    Refactor - create look up hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -15,7 +15,7 @@ const usePlatforms = () => {
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24
-    initialData: { count: platforms.length, next: null, results: platforms },
+    initialData: platforms,
   });
 }; //useData<Platform>("/platforms/lists/parents");
 
