560654e09091cdbbd40d2c603fa5d5ba38a81d2b
SoulBroCode <linhhuynh2000@gmail.com>
2023-11-03 22:24:19 +0000

    Handle Route Error, invalid page

--- src/services/routes.tsx
+++ src/services/routes.tsx
@@ -3,11 +3,13 @@ import GameDetailPage from "../pages/GameDetailPage";
 import Body from "../Body";
 import HomePage from "../pages/HomePage";
 import Layout from "../pages/Layout";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "",
