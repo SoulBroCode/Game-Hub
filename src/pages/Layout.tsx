908b148b7e2e21228b9b86d6565ba26a9d95f2e2
SoulBroCode <linhhuynh2000@gmail.com>
2023-11-04 01:04:22 +0000

    Organise Import

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
-import { Box } from "@chakra-ui/react";
 
 const Layout = () => {
   return (
